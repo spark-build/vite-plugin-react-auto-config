c63dc07e1a28a8a4ad0d650f357d01de857367a0
植成樑 <atzcl0310@gmail.com>
2022-01-04 16:39:09 +0800

    refactor: generateConfigFile to async

--- src/Bootstrap.ts
+++ src/Bootstrap.ts
@@ -23,27 +23,25 @@ export class Bootstrap {
     });
   }
 
-  // TODO: 可以改成异步 
-  private generateConfigFile() {
+  private async generateConfigFile() {
     assert(fs.existsSync(this.userConfigFilePath), '请在根目录上创建配置文件');
 
     const configTemporaryFilePath = this.server.resolveTmpPath(`config.js`);
 
     // 先将配置文件编译成 CommonJS
-    const source = fs.readFileSync(this.userConfigFilePath);
-
-    const result = esbuild.transformSync(source.toString(), {
-      sourcefile: this.userConfigFilePath,
-      format: 'cjs',
+    await esbuild.build({
+      entryPoints: [this.userConfigFilePath],
+      bundle: true,
+      platform: 'node',
+      treeShaking: true,
+      outfile: configTemporaryFilePath,
     });
-
-    fs.writeFileSync(configTemporaryFilePath, result.code);
 
     return configTemporaryFilePath;
   }
 
-  private getConfig() {
-    const configTemporaryFilePath = this.generateConfigFile();
+  private async getConfig() {
+    const configTemporaryFilePath = await this.generateConfigFile();
 
     // 删除可能存在的缓存
     delete require.cache[require.resolve(configTemporaryFilePath)];
@@ -53,13 +51,13 @@ export class Bootstrap {
     return require(configTemporaryFilePath).default;
   }
 
-  init() {
+  async init() {
     const cwd = process.cwd();
 
     this.userConfigPath = join(cwd, 'config');
     this.userConfigFilePath = join(this.userConfigPath, `index.ts`);
 
-    const userConfig = this.getConfig();
+    const userConfig = await this.getConfig();
 
     this.server
       .setEnv(userConfig.env || process.env.NODE_ENV || 'development')
@@ -76,7 +74,7 @@ export class Bootstrap {
     this.server.logger.printCompiling();
 
     if (isReCompile) {
-      this.server.setUserConfig(this.getConfig());
+      this.server.setUserConfig(await this.getConfig());
     }
 
     await this.server.run(isReCompile);
